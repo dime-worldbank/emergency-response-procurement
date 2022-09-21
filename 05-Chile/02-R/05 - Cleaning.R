aa09f80f8676123f750c451d771d1388ce07518b
Ruggio96 <rdoino@worldbank.org>
2022-09-21 14:05:36 -0400

    save

--- 05-Chile/02-R/05 - Cleaning.R
+++ 05-Chile/02-R/05 - Cleaning.R
@@ -130,6 +130,9 @@
       dplyr::group_by(lot_id)  %>% 
       dplyr::summarise(offer_total_price_award = sum(offer_total_price_award))
     
+    # We can add the just above information to the item-level data frame
+    data_lot <- left_join(data_lot, tot_purchased, by = "lot_id")
+    
   }
 
 # TREATMENT VS CONTROL ----------------------------------------------
@@ -142,6 +145,7 @@
      
      # Label the tenders that can be matched with the e-procurement data
      tender_covid$tender_covid_bin <- ifelse(tender_covid$ID %in% data_tender$tender_code, "Matched", "Unmatched")
+     tender_covid$item_match       <- ifelse(tender_covid$ID %in% item_covid$ITEM_ID, "Matched", "Unmatched")
      
      # Label 2022 tenders
      tender_covid$year_2022        <- ifelse(tender_covid$ID %in% data_tender_2022$tender_code, 1, 0)
@@ -180,7 +184,7 @@
          # add the variable based on the ID from the COVID-19 data set
          data <- data %>%
            mutate(
-             tender_covid_19   = ifelse(tender_code %in% tender_covid$ID                   , 1, 0) ,
+             tender_covid_19   = ifelse(tender_code %in% item_covid_list$ID                   , 1, 0) ,
              item_covid_19     = ifelse(lot_code_onu %in% item_covid_list$poiGoodAndService, 1, 0) ,
              medical_equipment = ifelse(substr(data$lot_code_onu, 0, 2) == 42              , 1, 0)
            )
@@ -211,8 +215,6 @@
     
     # We sub-sample and save each data frame
     for (level in c("tender","lot","offer")) {
-      
-      if (level == "tender" | level == "lot") {
         
         # load the data
         assign("data", get(paste0("data_", level)))
@@ -226,27 +228,24 @@
         saveRDS(data, paste0(dropbox_dir, "2 - data_construct/", paste0("data_", level, "_sub.rds")))
         
         rm(data)
-        
-      } else {
-        
-        # load the data
-        assign("data", get(paste0("data_", level, "_usd")))
-        
-        # add the variable based on the ID from the COVID-19 data set
-        data <- data %>%
-          filter(
-            tender_code %in% list_tender_code_sub$tender_code
-          )
-        
-        saveRDS(data, paste0(dropbox_dir, "2 - data_construct/", paste0("data_", level, "_sub.rds")))
-        
-        rm(data)
-        
-      }
       
     }
     
   }
+
+tab_value_purchased_covid <- data_lot     %>% 
+  filter(year == "2020" | year == "2021") %>% 
+  filter(item_covid_19 == 1)              %>% 
+  dplyr::group_by(lot_code_onu)           %>% 
+  dplyr::summarise(value_purchased_covid = sum(offer_total_price_award, na.rm = TRUE))
+
+tab_value_purchased_covid_emergency <- data_lot     %>% 
+  filter(year == "2020" | year == "2021")           %>% 
+  filter(item_covid_19 == 1 & tender_covid_19 == 1) %>% 
+  dplyr::group_by(lot_code_onu)                     %>% 
+  dplyr::summarise(value_purchased_covid_emergency = sum(offer_total_price_award, na.rm = TRUE))
+
+tab_summary_item_covid <- merge(tab_value_purchased_covid, tab_value_purchased_covid_emergency, by = "lot_code_onu")
 
 
 # SAVING  -----------------------------------------------------------------
