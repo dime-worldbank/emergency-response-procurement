417b745216b3848ef7cd0a5f91ea6bad30f678bc
MariaArnalCanudo <ma1881@georgetown.edu>
2023-07-27 16:49:01 -0400

    Create summary_table.R

---
+++ 04-Honduras/02-R/2-data_contruct/Functions/summary_table.R
@@ -0,0 +1,23 @@
+
+summary_table <- function(data, vars) {
+  
+  data %>%  
+    summarise(
+      across({{ vars }},
+             list(
+               n    = ~ sum(!is.na(.x)),
+               mean = ~ mean(.x, na.rm = TRUE)       ,
+               sd   = ~ sd(.x, na.rm = TRUE)         ,
+               min  = ~ min(.x, na.rm = TRUE)        ,
+               max  = ~ max(.x, na.rm = TRUE)),
+             .names = "{.col}-{.fn}")) %>% 
+    pivot_longer(everything()) %>% 
+    separate(name, sep = "-", into = c("var","stat"))  %>% 
+    pivot_wider(names_from = "stat", values_from = "value") %>% 
+    mutate_if(
+      is.numeric,
+      funs(
+        format(., )
+      )
+    )
+}
